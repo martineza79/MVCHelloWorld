32456c36eb0a17d46996d20fefbe9349f6e67d31
ALEX MARTINEZ <martineza79@outlook.com>
2017-02-20 13:27:02 -0600

    Add project files.

---
+++ MVCHelloWorld/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using MVCHelloWorld.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCHelloWorld.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            var cars = HelloModel.GetCars();
+            ViewBag.HelloWorld = "Hello World";// This is the second way to render "Hello World" on the Index.cshtml view
+            return View(cars);
+        }
+    }
+}
---
+++ MVCHelloWorld/Models/HelloModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCHelloWorld.Models
+{
+    public class HelloModel
+    {
+        public static List<Car> GetCars()
+        {
+            List<Car> cars = new List<Car>
+            {
+            new Car { Id = 1, Make = "Cadillac", Model = "CTS"},
+            new Car { Id = 2, Make = "Ford", Model = "Mustang" },
+            new Car { Id = 3, Make = "Jeep", Model = "Cherokee" }};
+            return cars;
+        }
+    }
+
+    public class Car
+    {
+        public int Id { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+    }
+}
